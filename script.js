3e352a4f90dbfbeb1b08a31c8acb05743767b328
Erickwfg <ericksaenz894@gmail.com>
2024-05-20 19:56:35 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let nose = document.getElementById('register');
+let nose2 = document.getElementById('sesion');
+let tampoco = document.getElementById('hidden1');
+let tampoco2 = document.getElementById('hidden2');
+nose.addEventListener('click', ()=>{
+    // tampoco.style.display = 'flex';
+    tampoco.style.left = '40%';
+    tampoco.style.opacity = '1';
+})
+nose2.addEventListener('click', ()=>{
+    // tampoco2.style.display = 'flex';
+    tampoco2.style.left = '40%';
+    tampoco2.style.opacity = '1';
+})
