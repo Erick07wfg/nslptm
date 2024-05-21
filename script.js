eae2a3a9a16265bb31ecc59dc59ca87534e5da7e
Erick07wfg <147286346+Erick07wfg@users.noreply.github.com>
2024-05-20 20:21:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-let nose = document.getElementById('register');
-let nose2 = document.getElementById('sesion');
-let tampoco = document.getElementById('hidden1');
-let tampoco2 = document.getElementById('hidden2');
+let nose = document.getElementById('register')
+let nose2 = document.getElementById('sesion')
+let tampoco = document.getElementById('hidden1')
+let tampoco2 = document.getElementById('hidden2')
+let menos = document.getElementById('conf_register')
+let menos2 = document.getElementById('canc_register')
+let niidea = document.getElementById('conf_sesion')
+let niidea2 = document.getElementById('canc_sesion')
 nose.addEventListener('click', ()=>{
-    // tampoco.style.display = 'flex';
-    tampoco.style.left = '40%';
-    tampoco.style.opacity = '1';
+    tampoco.style.left = '40%'
+    tampoco.style.opacity = '1'
 })
 nose2.addEventListener('click', ()=>{
-    // tampoco2.style.display = 'flex';
-    tampoco2.style.left = '40%';
-    tampoco2.style.opacity = '1';
+    tampoco2.style.left = '40%'
+    tampoco2.style.opacity = '1'
+})
+menos.addEventListener('click', ()=>{
+    tampoco.style.left = '70%'
+    tampoco.style.opacity = '0'
+})
+menos2.addEventListener('click', ()=>{
+    tampoco.style.left = '70%'
+    tampoco.style.opacity = '0'
+})
+niidea.addEventListener('click', ()=>{
+    tampoco2.style.left = '10%'
+    tampoco2.style.opacity = '0'
+})
+niidea2.addEventListener('click', ()=>{
+    tampoco2.style.left = '10%'
+    tampoco2.style.opacity = '0'
 })
